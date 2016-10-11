3247453a6b5c9bfb2fa12151214de1bd0ff2f8e0
NeelP1 <pi@raspberrypi.(none)>
2016-10-11 12:57:43 +0000

    Added essential apps to install

--- install-apps.sh
+++ install-apps.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+#Essentials
+
+sudo apt-get install git
+
 sudo apt-get install tightvncserver
+
+sudo apt-get install chromium-browser
+
+sudo apt-get update
+
+
+#Python
+
+
+#Embedded programming
+
+git clone git://github.com/adafruit/Adafruit-Raspberry-Pi-Python-Code.git
+
